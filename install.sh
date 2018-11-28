6dd0ea1a409c7762c02f54a0a48375fe5ba408c1
Leonardo Medeiros <leostayner@gmail.com>
2018-11-28 13:15:51 -0200

    load update

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ sudo apt-get -y update
 sudo apt-get install -y python3-pip
 sudo pip3 install flask
 sudo pip3 install flask_restful
+sudo pip3 install boto3
 export APP_URL="0.0.0.0"
 python3 App.py
--- load.py
+++ load.py
@@ -2,29 +2,33 @@ import os
 from flask import Flask, render_template, jsonify
 from flask_restful import Api, Resource, reqparse, fields, marshal, abort
 import random
+import boto3
 
 app = Flask(__name__)
 api = Api(app)
 
-dic_id   = []
+dic_id   = {}
 list_ids = []
 
-def list_id(ec2):
-    list_instances = ec2.instances.all()
-    
+def list_id(ec2, dic_id, list_ids):
+    list_instances = ec2.instances.filter(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['running']}])
+
     for instance in list_instances:
         list_tags = (instance.tags)
 
-        try:
-            for tag in list_tags:
-                if (tag["Key"] == "Owner") and (tag["Value"] == "Leonardo Medeiros") and len(list_tags) == 1:
-                    list_id.append[instance.id]
-                    dic[instance.id] = instance.public_ip_address)
-        except:
-            pass
-
+        for tag in list_tags:
+            if (tag["Key"] == "Owner") and (tag["Value"] == "Leonardo Medeiros") and len(list_tags) == 1:
+                list_ids.append(str(instance.id))
+                dic_id[instance.id] = instance.public_ip_address
+                
+    return dic_id, list_id
+    
 ec2 = boto3.resource('ec2')
-list_id(ec2)
+#ec2 = boto3.resource('ec2', region_name = "us-east-1" , aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY)
+dic_id, list_id = list_id(ec2, dic_id, list_ids)
+
 
 class GetAll(Resource):
     def __init__(self):
